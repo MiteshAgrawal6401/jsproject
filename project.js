dacb567da3d1bf799eca3f5ba84394a79dde7aba
Mitesh <miteshagrawal6401@gmail.com>
2023-02-24 15:42:12 +0530

    completed eventlistner

--- project.js
+++ project.js
@@ -9,10 +9,27 @@ for(var i=1; i<=16; i++){
     d.className += " flex"; 
 for(var j=1;j<=15;j++){
     const box1 = document.createElement("div");
-    box1.id="column";
+    let column_name=("row"+i+" -"+j);
+    box1.id=column_name;
     document.getElementById(row_name).appendChild(box1);
+
+    let b= document.getElementById(column_name);
+    b.className += " block"; 
+
+    
 }
 }
+
+let element=document.getElementsByClassName(" block");
+
+for( let i=0 ; i<element.length; i++){
+    element[i].addEventListener("click",function(){mark(i)});
+}
+    
+function mark(value){
+    element[value].style.backgroundColor="black";
+}
+
 
 
 
