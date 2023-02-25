9ac809d040d74470bac5c2bf420d0a4861830c11
Angaj <trancidence@gmail.com>
2023-02-25 20:08:05 +0530

    added event listeners sucessfuly

--- project.js
+++ project.js
@@ -7,16 +7,27 @@ for(var i=1; i<=16; i++){
     
     let d = document.getElementById(row_name);
     d.className += " flex"; 
-for(var j=1;j<=16;j++){
-    const box1 = document.createElement("div");
-    let blockname = (row_name +" - "+j); 
-    box1.id = blockname; 
-    document.getElementById(row_name).appendChild(box1);
 
-    let a = document.getElementById(blockname);
-    a.className += " block";  
-}
+    for(var j=1;j<=16;j++){
+        const box1 = document.createElement("div");
+        let blockname = (row_name +" - "+j); 
+        box1.id = blockname; 
+        document.getElementById(row_name).appendChild(box1);
+
+        let a = document.getElementById(blockname);
+        a.className += "block";  
+    }
 }
 
+let elements = document.getElementsByClassName("block");
+
+for(let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function() { colour(i) }); 
+}
+
+
+function colour(value) {
+    elements[value].style.backgroundColor="black";
+}
 
 
